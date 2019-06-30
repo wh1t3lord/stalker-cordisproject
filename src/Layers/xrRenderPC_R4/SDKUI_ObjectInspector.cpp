029f49f3fd2ee3f63082787b521658cede36828e
Lord <phantom1020@yandex.ru>
2019-06-30 17:19:10 +0300

    Start implementing Object inspector widget. In the next time you must solve problem with CSE_Abstract class in xrRender_R4 project

--- src/Layers/xrRenderPC_R4/SDKUI_ObjectInspector.cpp
+++ src/Layers/xrRenderPC_R4/SDKUI_ObjectInspector.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "SDKUI_ObjectInspector.h"
+#include "SDKUI_RightWindow.h"
 
 namespace Cordis
 {
@@ -15,7 +16,66 @@ void SDKUI_ObjectInspector::Draw(void)
     if (ImGui::Begin("Object Properties", &this->m_is_visible, this->m_flag))
     {
         // @ Lord -> Затем добавить вертикальный separator (таблицу)
-        
+		switch (SDKUI_RightWindow::Widget().GetSection())
+		{
+		case kSection_SpawnElements:
+		{
+			break;
+		}
+		case kSection_WayPoints:
+		{
+			break;
+		}
+		case kSection_StaticGeometry:
+		{
+			
+			break;
+		}
+		case kSection_Lights:
+		{
+			break;
+		}
+		case kSection_Sectors:
+		{
+			break;
+		}
+		case kSection_Portals:
+		{
+			break;
+		}
+		case kSection_SoundEnvironment:
+		{
+			break;
+		}
+		case kSection_SoundSource:
+		{
+			break;
+		}
+		case kSection_AIMap:
+		{
+			break;
+		}
+		case kSection_Groups:
+		{
+			break;
+		}
+		case kSection_StaticParticles:
+		{
+			break;
+		}
+		case kSection_FogVolumes:
+		{
+			break;
+		}
+		case kSection_Wallmarks:
+		{
+			break;
+		}
+		case kSection_DetailObjects:
+		{
+			break;
+		}
+		}
     }
 	ImGui::End();
 }
