0b4872778d79c012c576030eeafb9f47ef363bdf
phantom1020 <phantom1020@yandex.ru>
2019-08-06 12:46:27 +0300

    Start implementing Logic Manager

---
+++ src/xrGame/Script_LogicManager.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Script_LogicEntity.h"
+
+namespace Cordis
+{
+namespace Scripts
+{
+class Script_LogicManager
+{
+private:
+    Script_LogicManager(void) {}
+
+public:
+    inline static Script_LogicManager& getInstance(void) noexcept
+    {
+        static Script_LogicManager instance;
+        return instance;
+    }
+
+    ~Script_LogicManager(void)
+    {
+        for (Script_ILogicEntity* it : this->m_data)
+        {
+            if (it)
+            {
+                delete it;
+                it = nullptr;
+            }
+        }
+
+        this->m_data.clear();
+    }
+
+    inline void All_Deativate(void)
+    {
+        for (Script_ILogicEntity* it : this->m_data)
+        {
+
+        }
+    }
+
+private:
+    xr_vector<Script_ILogicEntity*> m_data;
+};
+} // namespace Scripts
+} // namespace Cordis
