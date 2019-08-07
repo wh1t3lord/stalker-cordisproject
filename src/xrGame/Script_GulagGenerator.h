60a4444fead4ae7051f1a47f885a7f365ac83e8b
phantom1020 <phantom1020@yandex.ru>
2019-08-07 17:59:10 +0300

    Start implementing load_job function

--- src/xrGame/Script_GulagGenerator.h
+++ src/xrGame/Script_GulagGenerator.h
@@ -6,13 +6,16 @@ namespace Scripts
 {
 namespace GulagGenerator
 {
-    inline static xr_string& getLtx(void) noexcept
-    {
-        static xr_string instance;
-        return instance;
-    }
+inline static xr_string& getLtx(void) noexcept
+{
+    static xr_string instance = "";
+    return instance;
+}
 
-    
+bool load_job(Script_SE_SmartTerrain* smart) 
+{
+
+}
 
 } // namespace GulagGenerator
 } // namespace Scripts
