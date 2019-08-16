0bf137ac9728886770f9976d4886a6e96016ccaf
phantom1020 <phantom1020@yandex.ru>
2019-08-16 12:19:52 +0300

    Added in GlobalHelper maps from Script_SE_SmartCover. Added members for this class.

--- src/xrGame/Script_SE_SmartCover.cpp
+++ src/xrGame/Script_SE_SmartCover.cpp
@@ -5,5 +5,21 @@ namespace Cordis
 {
 namespace Scripts
 {
+Script_SE_SmartCover::Script_SE_SmartCover(LPCSTR section_name) : inherited(section_name) {}
+Script_SE_SmartCover::~Script_SE_SmartCover(void) {}
+void Script_SE_SmartCover::STATE_Write(NET_Packet& Packet) 
+{ inherited::STATE_Write(Packet); }
+
+void Script_SE_SmartCover::STATE_Read(NET_Packet& Packet, std::uint16_t size) 
+{ inherited::STATE_Read(Packet, size); }
+
+void Script_SE_SmartCover::on_before_register(void) 
+{ }
+
+void Script_SE_SmartCover::on_register(void) 
+{
+    
 }
+void Script_SE_SmartCover::on_unregister(void) {}
+} // namespace Scripts
 } // namespace Cordis
