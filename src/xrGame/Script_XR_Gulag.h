2d32a56499b4266263b0cadd48f46156f300277f
phantom1020 <phantom1020@yandex.ru>
2019-08-18 11:47:41 +0300

    Implement XR_GULAG::job_in_restrictor function

--- src/xrGame/Script_XR_Gulag.h
+++ src/xrGame/Script_XR_Gulag.h
@@ -31,7 +31,7 @@ inline CSE_ALifeDynamicObject* get_npc_smart(CScriptGameObject* object)
 
     return nullptr;
 }
-// Lord: тогда прочекать за удаление то есть здесь уже Singlton 
+// Lord: тогда прочекать за удаление то есть здесь уже Singlton
 inline static xr_map<xr_string, CInifile*>& getDynamicLtx(void) noexcept
 {
     static xr_map<xr_string, CInifile*> instance;
@@ -57,6 +57,49 @@ inline CInifile loadLtx(const xr_string& name)
     return CInifile("system.ltx");
 }
 
+inline bool is_job_in_restrictor(
+    Script_SE_SmartTerrain* smart, const xr_string& restrictor_name, const xr_string& waypoint_name)
+{
+    if (!smart)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    if (!restrictor_name.size())
+    {
+        R_ASSERT2(false, "string can't be empty!");
+        return false;
+    }
+
+    if (!waypoint_name.size())
+    {
+        R_ASSERT2(false, "string can't be empty!");
+        return false;
+    }
+
+    CScriptGameObject* restrictor_object = DataBase::Storage::getInstance().getZoneByName()[restrictor_name];
+
+    if (!restrictor_object)
+    {
+        // @ Lord: имеет ли смысл ставить сюда ассерт?
+        Msg("[Scripts/XR_GULAG/is_job_in_restrictor(smart, restrictor_name, waypoint_name)] WARNING: object doesn't "
+            "spawn yet! You're trying to access it!!!");
+        return false;
+    }
+
+    CPatrolPathParams patrol = CPatrolPathParams(waypoint_name.c_str());
+    const std::uint32_t& total_count = patrol.count();
+
+    for (std::uint32_t i = 0; i < total_count; ++i)
+    {
+        if (!restrictor_object->inside(patrol.point(i)))
+            return false;
+    }
+    
+    return true;
+}
+
 } // namespace XR_GULAG
-} // namespace Script
+} // namespace Scripts
 } // namespace Cordis
