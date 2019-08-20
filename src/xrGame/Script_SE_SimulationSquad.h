1d939ac1121fd2e322193acb76a2abb935eabcb1
phantom1020 <phantom1020@yandex.ru>
2019-08-20 15:19:14 +0300

    Getter setter to SmartTerrainID

--- src/xrGame/Script_SE_SimulationSquad.h
+++ src/xrGame/Script_SE_SimulationSquad.h
@@ -2,7 +2,6 @@
 
 #include "xrServer_Objects_ALife_Monsters.h"
 
-
 namespace Cordis
 {
 namespace Scripts
@@ -10,10 +9,17 @@ namespace Scripts
 class Script_SE_SimulationSquad : public CSE_ALifeOnlineOfflineGroup
 {
     using inherited = CSE_ALifeOnlineOfflineGroup;
+
 public:
     Script_SE_SimulationSquad(LPCSTR section);
     virtual ~Script_SE_SimulationSquad(void);
+
+    inline std::uint32_t getSmartTerrainID(void) noexcept { return this->m_smart_terrain_id; }
+    inline void setSmartTerrainID(const std::uint32_t& value) noexcept { this->m_smart_terrain_id = value; }
+
+private:
+    std::uint32_t m_smart_terrain_id;
 };
 
-}
+} // namespace Scripts
 } // namespace Cordis
