dcf7319165133ec1e15b79138bc16a01685b1e1f
phantom1020 <phantom1020@yandex.ru>
2019-08-20 15:17:42 +0300

    Start implementing Script_SE_SimulationSquad

--- src/xrGame/Script_SE_SimulationSquad.cpp
+++ src/xrGame/Script_SE_SimulationSquad.cpp
@@ -5,6 +5,16 @@ namespace Cordis
 {
 namespace Scripts
 {
+Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section) : inherited(section) 
+{
 
 }
+
+Script_SE_SimulationSquad::~Script_SE_SimulationSquad(void) 
+{
+
+}
+
+
+} // namespace Scripts
 } // namespace Cordis
