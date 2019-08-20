901cab83a61bb71f2efb81f9e96bbd0cbd06b734
phantom1020 <phantom1020@yandex.ru>
2019-08-20 15:23:48 +0300

    Implement another getters

--- src/xrGame/Script_SE_SimulationSquad.h
+++ src/xrGame/Script_SE_SimulationSquad.h
@@ -16,13 +16,23 @@ public:
 
     inline std::uint32_t getSmartTerrainID(void) noexcept { return this->m_smart_terrain_id; }
     inline std::uint32_t getCurrentSpotID(void) noexcept { return this->m_current_spot_id; }
+    inline std::uint32_t getCurrentTargetID(void) noexcept { return this->m_current_target_id; }
+    inline std::uint32_t getAssignedTargetID(void) noexcept { return this->m_assigned_target_id; }
+    inline xr_string& getSettingsID(void) noexcept { return this->m_settings_id; }
+
 
     inline void setSmartTerrainID(const std::uint32_t& value) noexcept { this->m_smart_terrain_id = value; }
     inline void setCurrentSpotID(const std::uint32_t& value) noexcept { this->m_current_spot_id = value; }
+    inline void setCurrentTargetID(const std::uint32_t& value) noexcept { this->m_current_target_id = value; }
+    inline void setAssignedTargetID(const std::uint32_t& value) noexcept { this->m_assigned_target_id = value; }
 
 private:
     std::uint32_t m_smart_terrain_id;
     std::uint32_t m_current_spot_id;
+    std::uint32_t m_current_target_id;
+    std::uint32_t m_assigned_target_id;
+
+    xr_string m_settings_id;
 };
 
 } // namespace Scripts
