688f878402f76e15129a05d8a3b7205ff312a92a
phantom1020 <phantom1020@yandex.ru>
2019-08-20 15:39:47 +0300

    Half of total initialization in Script_SE_SimulationSquad

--- src/xrGame/Script_SE_SimulationSquad.cpp
+++ src/xrGame/Script_SE_SimulationSquad.cpp
@@ -1,13 +1,24 @@
 #include "stdafx.h"
 #include "Script_SE_SimulationSquad.h"
+#include "Script_XR_Logic.h"
+
 
 namespace Cordis
 {
 namespace Scripts
 {
-Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section) : inherited(section) 
+Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section) : inherited(section), m_assigned_target_id(0), m_current_spot_id(0), m_current_target_id(0), m_smart_terrain_id(0),
+      m_settings_id(this->name()),
+      m_sound_manager(Script_SoundManager::getSoundManager((xr_string("squad_").append(this->name())))),
+      m_player_id(Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "faction")),
+      m_condlist_action(XR_LOGIC::parse_condlist_by_server_object("assign_action", "target_smart",
+          Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "target_smart"))),
+      m_condlist_death(XR_LOGIC::parse_condlist_by_server_object("death_condlist", "on_death",
+          Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "target_smart"))),
+      m_condlist_invulnerability(XR_LOGIC::parse_condlist_by_server_object("invulnerability", "invulnerability", Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "invulnerability"))),
+      
 {
-
+    
 }
 
 Script_SE_SimulationSquad::~Script_SE_SimulationSquad(void) 
--- src/xrGame/Script_SE_SimulationSquad.h
+++ src/xrGame/Script_SE_SimulationSquad.h
@@ -14,25 +14,49 @@ public:
     Script_SE_SimulationSquad(LPCSTR section);
     virtual ~Script_SE_SimulationSquad(void);
 
+
+
+
+
+
+
+
+#pragma region Cordis Getters
     inline std::uint32_t getSmartTerrainID(void) noexcept { return this->m_smart_terrain_id; }
     inline std::uint32_t getCurrentSpotID(void) noexcept { return this->m_current_spot_id; }
     inline std::uint32_t getCurrentTargetID(void) noexcept { return this->m_current_target_id; }
     inline std::uint32_t getAssignedTargetID(void) noexcept { return this->m_assigned_target_id; }
     inline xr_string& getSettingsID(void) noexcept { return this->m_settings_id; }
+#pragma endregion
 
 
+#pragma region Cordis Setters
     inline void setSmartTerrainID(const std::uint32_t& value) noexcept { this->m_smart_terrain_id = value; }
     inline void setCurrentSpotID(const std::uint32_t& value) noexcept { this->m_current_spot_id = value; }
     inline void setCurrentTargetID(const std::uint32_t& value) noexcept { this->m_current_target_id = value; }
     inline void setAssignedTargetID(const std::uint32_t& value) noexcept { this->m_assigned_target_id = value; }
+#pragma endregion
+
+
+    
+
 
 private:
+    bool m_is_always_walk;
+    bool m_is_always_arrived;
     std::uint32_t m_smart_terrain_id;
     std::uint32_t m_current_spot_id;
     std::uint32_t m_current_target_id;
     std::uint32_t m_assigned_target_id;
-
+    std::uint32_t m_sympathy;
+    xr_map<std::uint32_t, CondlistData> m_condlist_action;
+    xr_map<std::uint32_t, CondlistData> m_condlist_death;
+    xr_map<std::uint32_t, CondlistData> m_condlist_invulnerability;
+    xr_map<std::uint32_t, CondlistData> m_condlist_show_spot;
+    xr_string m_relationship;
+    xr_string m_player_id;
     xr_string m_settings_id;
+    Script_SoundManager m_sound_manager;
 };
 
 } // namespace Scripts
