340d3c3520a22a656f21838777833d0cb520c42f
phantom1020 <phantom1020@yandex.ru>
2019-08-20 19:32:00 +0300

    Added goodwill instance to DataBase::Storage

--- src/xrGame/Script_SE_SimulationSquad.cpp
+++ src/xrGame/Script_SE_SimulationSquad.cpp
@@ -9,7 +9,8 @@ namespace Cordis
 {
 namespace Scripts
 {
-Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section) : inherited(section), m_assigned_target_id(0), m_current_spot_id(0), m_current_target_id(0), m_smart_terrain_id(0),
+Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section)
+    : inherited(section), m_assigned_target_id(0), m_current_spot_id(0), m_current_target_id(0), m_smart_terrain_id(0),
       m_settings_id(this->name()),
       m_sound_manager(Script_SoundManager::getSoundManager((xr_string("squad_").append(this->name())))),
       m_player_id(Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "faction")),
@@ -17,23 +18,23 @@ Script_SE_SimulationSquad::Script_SE_SimulationSquad(LPCSTR section) : inherited
           Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "target_smart"))),
       m_condlist_death(XR_LOGIC::parse_condlist_by_server_object("death_condlist", "on_death",
           Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "target_smart"))),
-      m_condlist_invulnerability(XR_LOGIC::parse_condlist_by_server_object("invulnerability", "invulnerability", Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "invulnerability"))),
+      m_condlist_invulnerability(XR_LOGIC::parse_condlist_by_server_object("invulnerability", "invulnerability",
+          Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "invulnerability"))),
       m_relationship(Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "relationship")),
       m_sympathy(Globals::Utils::cfg_get_number(Globals::get_system_ini(), this->m_settings_id, "sympathy", this)),
-      m_condlist_show_spot(XR_LOGIC::parse_condlist_by_server_object("show_spot", "show_spot", Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "show_spot"))),
-      m_is_always_arrived(Globals::Utils::cfg_get_bool(Globals::get_system_ini(), this->m_settings_id, "always_arrived", this)),
-      m_is_always_walk(Globals::Utils::cfg_get_bool(Globals::get_system_ini(), this->m_settings_id, "always_walk", this))
+      m_condlist_show_spot(XR_LOGIC::parse_condlist_by_server_object("show_spot", "show_spot",
+          Globals::Utils::cfg_get_string(Globals::get_system_ini(), this->m_settings_id, "show_spot"))),
+      m_is_always_arrived(
+          Globals::Utils::cfg_get_bool(Globals::get_system_ini(), this->m_settings_id, "always_arrived", this)),
+      m_is_always_walk(
+          Globals::Utils::cfg_get_bool(Globals::get_system_ini(), this->m_settings_id, "always_walk", this))
 {
-    
 }
 
-Script_SE_SimulationSquad::~Script_SE_SimulationSquad(void) 
-{
-
-}
+Script_SE_SimulationSquad::~Script_SE_SimulationSquad(void) {}
 
 void Script_SE_SimulationSquad::set_location_types_section(const xr_string& section)
-{ 
+{
     if (locations_ini.section_exist(section.c_str()))
     {
         pcstr N;
@@ -44,6 +45,20 @@ void Script_SE_SimulationSquad::set_location_types_section(const xr_string& sect
     }
 }
 
+void Script_SE_SimulationSquad::set_squad_sympathy(const std::uint32_t& sympathy)
+{
+    std::uint32_t _sympathy = sympathy ? sympathy : this->m_sympathy;
+
+    if (_sympathy)
+    {
+        for (AssociativeVector<std::uint16_t, CSE_ALifeMonsterAbstract*>::const_iterator it =
+                 this->squad_members().begin();
+             it != this->squad_members().end(); ++it)
+        {
+            
+        }
+    }
+}
 
 } // namespace Scripts
 } // namespace Cordis
