e59b46f9a179b3acf2af46e950fb26bd376d8cb8
phantom1020 <phantom1020@yandex.ru>
2019-08-23 18:05:03 +0300

    I'm out. Continue tomorrow

---
+++ src/xrGame/Script_TreasureManager.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "Script_TreasureManager.h"
+
+namespace Cordis
+{
+namespace Scripts
+{
+Script_TreasureManager::~Script_TreasureManager(void) {}
+} // namespace Scripts
+} // namespace Cordis
