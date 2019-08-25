6c60f0fe4d9b6ccd09dd8f8c4948fb93c30c6361
phantom1020 <phantom1020@yandex.ru>
2019-08-25 09:11:31 +0300

    Implement method register_item in Script_TreasureManager

--- src/xrGame/Script_TreasureManager.cpp
+++ src/xrGame/Script_TreasureManager.cpp
@@ -39,15 +39,55 @@ bool Script_TreasureManager::fill(CSE_ALifeDynamicObject* server_object, const x
         }
         else
         {
-            Msg("[Scripts/Script_TreasureManager/fill(server_object, treasure_id_name)] Attempt to register unknown item [%s] in secret [%s]", server_object->name_replace(), treasure_id_name.c_str());
+            Msg("[Scripts/Script_TreasureManager/fill(server_object, treasure_id_name)] Attempt to register unknown "
+                "item [%s] in secret [%s]",
+                server_object->name_replace(), treasure_id_name.c_str());
             R_ASSERT(false);
         }
     }
     else
     {
-        Msg("[Scripts/Script_TreasureManager/fill(server_object, treasure_id_name)] Attempt to register item [%s] in unexistent secret [%s]",
+        Msg("[Scripts/Script_TreasureManager/fill(server_object, treasure_id_name)] Attempt to register item [%s] in "
+            "unexistent secret [%s]",
             server_object->name_replace(), treasure_id_name.c_str());
         R_ASSERT(false);
+    }
+
+    return false;
+}
+
+bool Script_TreasureManager::register_item(CSE_ALifeDynamicObject* server_object)
+{
+    if (!server_object)
+    {
+        R_ASSERT2(false, "object was null!");
+        return;
+    }
+
+    if (server_object->spawn_ini().section_exist("secret"))
+    {
+        const char* Field;
+        const char* Value;
+
+        if (server_object->spawn_ini().r_line("secret", 0, &Field, &Value))
+        {
+            if (strcmp(Field, "name"))
+            {
+                Msg("[Scripts/Scripts_TreasureManager/register_item(server_object)] There is no 'name' field in "
+                    "[secret] section for object [%s]",
+                    server_object->name_replace());
+                R_ASSERT(false);
+            }
+
+            if (!strlen(Value))
+            {
+                Msg("[Scripts/Scripts_TreasureManager/register_item(server_object)] Field 'name' in [secret] section got no value for object [%s]",
+                    server_object->name_replace());
+                R_ASSERT(false);
+            }
+
+            return this->fill(server_object, Value);
+        }
     }
 
     return false;
