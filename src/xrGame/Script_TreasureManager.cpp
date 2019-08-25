dd97ba95f921e1f1d12637b38f73a90643fcc2c7
phantom1020 <phantom1020@yandex.ru>
2019-08-25 08:59:14 +0300

    Rewrite method fill in Script_TreasureManager

--- src/xrGame/Script_TreasureManager.cpp
+++ src/xrGame/Script_TreasureManager.cpp
@@ -6,5 +6,41 @@ namespace Cordis
 namespace Scripts
 {
 Script_TreasureManager::~Script_TreasureManager(void) {}
+
+bool Script_TreasureManager::fill(CSE_ALifeDynamicObject* server_object, const xr_string& treasure_id_name)
+{
+    if (!server_object)
+    {
+        R_ASSERT2(false, "object was null!");
+        return;
+    }
+
+    if (!treasure_id_name.size())
+    {
+        R_ASSERT2(false, "string can't be null!");
+        return;
+    }
+
+    if (this->m_secrets[treasure_id_name].m_items[server_object->name()].size())
+    {
+        xr_vector<std::pair<std::pair<std::uint32_t, std::uint32_t>, xr_vector<std::uint16_t>>>& data =
+            this->m_secrets[treasure_id_name].m_items[server_object->name()];
+
+        for (std::uint32_t i = 0; i < data.size(); ++i)
+        {
+            int count = data[i].second.size();
+
+            if (count < data[i].first.first)
+            {
+                data[i].second.push_back(server_object->ID);
+                return true;
+            }
+
+        }
+    }
+
+    return false;
+}
+
 } // namespace Scripts
 } // namespace Cordis
--- src/xrGame/Script_TreasureManager.h
+++ src/xrGame/Script_TreasureManager.h
@@ -7,12 +7,87 @@ namespace Scripts
 class Script_TreasureManager
 {
 private:
-    Script_TreasureManager(void) 
+    Script_TreasureManager(void)
     {
+        CInifile ini = CInifile("misc\\secrets.ltx");
+        std::uint32_t count_lines = ini.line_count("list");
 
+        for (std::uint32_t i = 0; i < count_lines; ++i)
+        {
+            const char* N;
+            const char* V;
+            if (ini.r_line("list", i, &N, &V))
+            {
+                if (ini.section_exist(N))
+                {
+                    DataSecret data;
+                    std::uint32_t item_count = ini.line_count(N);
+                    xr_string item_section_name = "";
+
+                    for (std::uint32_t j = 0; j < item_count; ++j)
+                    {
+                        const char* N1;
+                        const char* V1;
+
+                        if (ini.r_line(N, j, &N1, &V1))
+                        {
+                            if (!strcmp(N1, "empty"))
+                            {
+                                data.m_condlist_empty =
+                                    XR_LOGIC::parse_condlist_by_server_object("treasure_manager", "empty_cond", V1);
+                            }
+                            else if (!strcmp(N1, "refreshing"))
+                            {
+                                data.m_condlist_refreshing = XR_LOGIC::parse_condlist_by_server_object(
+                                    "treasure_manager", "refreshing_cond", V1);
+                            }
+                            else
+                            {
+                                xr_vector<std::pair<xr_string, std::uint32_t>> buffer =
+                                    Globals::Utils::parse_spawns(V1);
+
+                                if (!buffer.size())
+                                {
+                                    Msg("[Scripts/Script_TreasureManager/ctor] There is no items count set for "
+                                        "treasure [%s], item [%s]",
+                                        N, V1);
+                                    R_ASSERT(false);
+                                }
+
+                                for (std::pair<xr_string, std::uint32_t>& it : buffer)
+                                {
+                                    std::pair<std::pair<std::uint32_t, std::uint32_t>, xr_vector<std::uint16_t>> data;
+                                    data.first.first = atoi(it.first.c_str());
+                                    data.first.second = it.second ? it.second : 1;
+                                    this->m_secrets[N].m_items[V1].push_back(data);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Msg("[Scripts/Script_TreasureManager/ctor] There is no section [%s] in secrects.ltx", N);
+                    R_ASSERT(false);
+                }
+            }
+        }
+        // if (ini.section_exist())
     }
 
 public:
+    struct DataSecret
+    {
+        bool m_is_given;
+        bool m_is_checked;
+        std::uint8_t m_to_find;
+        xr_map<std::uint32_t, CondlistData> m_condlist_refreshing;
+        xr_map<std::uint32_t, CondlistData> m_condlist_empty;
+
+        // first -> count | second -> prob
+        xr_map<xr_string, xr_vector<std::pair<std::pair<std::uint32_t, std::uint32_t>, xr_vector<std::uint16_t>>>> m_items;
+    };
+
     inline static Script_TreasureManager& getInstance(void) noexcept
     {
         static Script_TreasureManager instance;
@@ -25,10 +100,14 @@ public:
     Script_TreasureManager(Script_TreasureManager&&) = delete;
     Script_TreasureManager& operator=(Script_TreasureManager&&) = delete;
 
+    bool fill(CSE_ALifeDynamicObject* server_object, const xr_string& treasure_id_name);
 
 private:
     bool m_is_items_spawned;
     xr_map<xr_string, std::uint16_t> m_secret_restrictors;
+    xr_map<std::uint16_t, xr_string> m_items_from_secrects;
+    xr_map<xr_string, DataSecret> m_secrets;
 };
+
 } // namespace Scripts
 } // namespace Cordis
