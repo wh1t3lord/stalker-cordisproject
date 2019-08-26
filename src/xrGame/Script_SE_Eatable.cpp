74957d31a05f120f5e317959d8a874a0a7d361b3
phantom1020 <phantom1020@yandex.ru>
2019-08-26 18:54:31 +0300

    Implemented Script_SE_Eatable

---
+++ src/xrGame/Script_SE_Eatable.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "Script_SE_Eatable.h"
+#include "Script_StoryObject.h"
+
+namespace Cordis
+{
+namespace Scripts
+{
+Script_SE_Eatable::Script_SE_Eatable(LPCSTR section_name) : inherited(section_name), m_is_secret_item(false) {}
+
+Script_SE_Eatable::~Script_SE_Eatable(void) {}
+
+void Script_SE_Eatable::on_register(void) 
+{
+    inherited::on_register(); 
+    Script_StoryObject::getInstance().check_spawn_ini_for_story_id(this);
+
+    this->m_is_secret_item = Script_TreasureManager::getInstance().register_item(this);
+}
+
+void Script_SE_Eatable::on_unregister(void) 
+{ Script_StoryObject::getInstance().unregistrate_by_id(this->ID);
+    inherited::on_unregister();
+}
+
+bool Script_SE_Eatable::can_switch_online(void) const 
+{ 
+    if (this->m_is_secret_item)
+        return false;
+
+    return inherited::can_switch_online(); 
+}
+} // namespace Scripts
+} // namespace Cordis
