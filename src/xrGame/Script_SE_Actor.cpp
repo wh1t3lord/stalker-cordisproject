17f0b55d5a7c1537f862896d25a9c85c2b40fb77
phantom1020 <phantom1020@yandex.ru>
2019-08-29 13:25:03 +0300

    Implemented default virtuals for Script_SE_Actor. Need implemnent native methods for this class

--- src/xrGame/Script_SE_Actor.cpp
+++ src/xrGame/Script_SE_Actor.cpp
@@ -24,6 +24,41 @@ void Script_SE_Actor::on_register(void)
     if (!this->m_is_start_position_filled)
     {
         Script_SimulationBoard::getInstance().fill_start_position();
+        this->m_is_start_position_filled = true;
+    }
+}
+
+void Script_SE_Actor::on_unregister(void)
+{
+    inherited::on_unregister();
+    Script_StoryObject::getInstance().unregistrate_by_id(this->ID);
+
+    Script_SimulationObjects::getInstance().unregistrate(this);
+}
+
+void Script_SE_Actor::STATE_Write(NET_Packet& packet)
+{
+    inherited::STATE_Write(packet);
+
+    Globals::set_save_marker(packet, Globals::kSaveMarkerMode_Save, false, "Script_SE_Actor");
+
+    packet.w_u8(this->m_is_start_position_filled ? 1 : 0);
+
+    Globals::set_save_marker(packet, Globals::kSaveMarkerMode_Save, true, "Scritp_SE_Actor");
+}
+
+void Script_SE_Actor::STATE_Read(NET_Packet& packet, std::uint16_t size)
+{
+    inherited::STATE_Read(packet, size);
+
+    if (FS.IsSDK())
+        return;
+
+    if (!DataBase::Storage::getInstance().getActor())
+    {
+        Globals::set_save_marker(packet, Globals::kSaveMarkerMode_Load, false, "Script_SE_Actor");
+        this->m_is_start_position_filled = packet.r_u8() ? true : false;
+        Globals::set_save_marker(packet, Globals::kSaveMarkerMode_Load, true, "Script_SE_Actor");
     }
 }
 
