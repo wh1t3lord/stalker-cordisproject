c436cc6fce19f14ca24d27f7c11bab1610339bcd
phantom1020 <phantom1020@yandex.ru>
2019-09-07 18:05:50 +0300

    Marked function is not implemneted

--- src/xrGame/Script_XR_Wounded.h
+++ src/xrGame/Script_XR_Wounded.h
@@ -8,6 +8,8 @@ namespace XR_WOUNDED
 {
 inline bool is_wounded(CScriptGameObject* npc)
 {
+    Msg("[Scripts/XR_WOUNDED/is_wounded(npc)] NOT IMPLEMENTED!");
+
     if (!npc)
     {
         R_ASSERT2(false, "object was null!");
