2a24da958fab197ae93645a0fab2557885741f60
phantom1020 <phantom1020@yandex.ru>
2019-09-07 09:11:21 +0300

    Started implemneting XR_CONDITIONS functions

--- src/xrGame/Script_XR_Condition.h
+++ src/xrGame/Script_XR_Condition.h
@@ -8,6 +8,93 @@ namespace Scripts
 namespace XR_CONDITION
 {
 inline bool is_surge_started(void) { return Script_SurgeManager::getInstance().IsStarted(); }
+
+inline bool is_enemy_actor(CScriptGameObject* enemy, CScriptGameObject* object)
+{
+    if (!enemy)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    if (!object)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    return (enemy->ID() == object->ID());
+}
+
+inline bool is_fighting_dist_ge(CScriptGameObject* enemy, CScriptGameObject* npc, const float& distance)
+{
+    if (!enemy)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    if (!npc)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    return (enemy->Position().distance_to_sqr(npc->Position()) >= (distance * distance));
+}
+
+inline bool is_fighting_dist_ge(CSE_ALifeDynamicObject* enemy, CSE_ALifeDynamicObject* npc, const float& distance)
+{
+    if (!enemy)
+    {
+        R_ASSERT2(false, "object was null!");
+        return;
+    }
+
+    if (!npc)
+    {
+        R_ASSERT2(false, "object was null!");
+        return;
+    }
+
+    return (enemy->Position().distance_to_sqr(npc->Position()) >= (distance * distance));
+}
+
+inline bool is_fighting_dist_le(CScriptGameObject* enemy, CScriptGameObject* npc, const float& distance)
+{
+    if (!enemy)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    if (!npc)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    return (enemy->Position().distance_to_sqr(npc->Position()) <= distance * distance);
+}
+
+inline bool is_fighting_dist_le(CSE_ALifeDynamicObject* enemy, CSE_ALifeDynamicObject* npc, const float& distance)
+{
+    if (!enemy)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    if (!npc)
+    {
+        R_ASSERT2(false, "object was null!");
+        return false;
+    }
+
+    return (enemy->Position().distance_to_sqr(npc->Position()) <= distance * distance);
+}
+
+
 } // namespace XR_CONDITION
 } // namespace Scripts
 } // namespace Cordis
