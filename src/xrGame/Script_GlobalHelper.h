a16492256fc1d3aa455d98f33485471bef5ddd63
phantom1020 <phantom1020@yandex.ru>
2019-10-12 17:58:16 +0300

    Initialized m_animpoint_table in Script_GlobalHelper

--- src/xrGame/Script_GlobalHelper.h
+++ src/xrGame/Script_GlobalHelper.h
@@ -1022,17 +1022,70 @@ private:
         this->setRegisteredHarmonicaVisuals("stalker_zombied_4", true);
         this->setRegisteredHarmonicaVisuals("stalker_neutral_nauchniy_face_2", true);
 
-        /*
-                this->m_animpoint_table["animpoint_stay_wall"] = {Globals::predicate_const_true, "animpoint_stay_wall"};
-                this->m_animpoint_table["animpoint_stay_wall"] = {Globals::predicate_animpoint_bread,
-           "animpoint_stay_wall_eat_bread"}; this->m_animpoint_table["animpoint_stay_wall"] =
-           {Globals::predicate_animpoint_kolbasa, "animpoint_stay_wall_eat_kolbasa"};
-                this->m_animpoint_table["animpoint_stay_wall"] = {Globals::predicate_animpoint_vodka,
-           "animpoint_stay_wall_drink_vodka"}; this->m_animpoint_table["animpoint_stay_wall"] =
-           {Globals::predicate_animpoint_energy, "animpoint_stay_wall_drink_energy"};
-                this->m_animpoint_table["animpoint_stay_wall"] = {Globals::predicate_animpoint_weapon,
-           "animpoint_stay_wall_weapon"};*/
+        this->m_animpoint_table["animpoint_stay_wall"] = {Globals::predicate_const_true, "animpoint_stay_wall"};
+        this->m_animpoint_table["animpoint_stay_wall"] = {
+            Globals::predicate_animpoint_bread, "animpoint_stay_wall_eat_bread"};
+        this->m_animpoint_table["animpoint_stay_wall"] = {
+            Globals::predicate_animpoint_kolbasa, "animpoint_stay_wall_eat_kolbasa"};
+        this->m_animpoint_table["animpoint_stay_wall"] = {
+            Globals::predicate_animpoint_vodka, "animpoint_stay_wall_drink_vodka"};
+        this->m_animpoint_table["animpoint_stay_wall"] = {
+            Globals::predicate_animpoint_energy, "animpoint_stay_wall_drink_energy"};
+        this->m_animpoint_table["animpoint_stay_wall"] = {
+            Globals::predicate_animpoint_weapon, "animpoint_stay_wall_weapon"};
 
+        this->m_animpoint_table["animpoint_stay_table"] = {Globals::predicate_const_true, "animpoint_stay_table"};
+        this->m_animpoint_table["animpoint_stay_table"] = {
+            Globals::predicate_animpoint_bread, "animpoint_stay_table_eat_bread"};
+        this->m_animpoint_table["animpoint_stay_table"] = {
+            Globals::predicate_animpoint_kolbasa, "animpoint_stay_table_eat_kolbasa"};
+        this->m_animpoint_table["animpoint_stay_table"] = {
+            Globals::predicate_animpoint_vodka, "animpoint_stay_table_drink_vodka"};
+        this->m_animpoint_table["animpoint_stay_table"] = {
+            Globals::predicate_animpoint_energy, "animpoint_stay_table_drink_energy"};
+        this->m_animpoint_table["animpoint_stay_table"] = {
+            Globals::predicate_animpoint_weapon, "animpoint_stay_table_weapon"};
+
+        this->m_animpoint_table["animpoint_sit_high"] = {Globals::predicate_const_true, "animpoint_sit_high"};
+        this->m_animpoint_table["animpoint_sit_high"] = {
+            Globals::predicate_animpoint_bread, "animpoint_sit_high_eat_bread"};
+        this->m_animpoint_table["animpoint_sit_high"] = {
+            Globals::predicate_animpoint_kolbasa, "animpoint_sit_high_eat_kolbasa"};
+        this->m_animpoint_table["animpoint_sit_high"] = {
+            Globals::predicate_animpoint_vodka, "animpoint_sit_high_drink_vodka"};
+        this->m_animpoint_table["animpoint_sit_high"] = {
+            Globals::predicate_animpoint_energy, "animpoint_sit_high_drink_energy"};
+        this->m_animpoint_table["animpoint_sit_high"] = {
+            Globals::predicate_animpoint_harmonica, "animpoint_sit_high_harmonica"};
+
+        this->m_animpoint_table["animpoint_sit_normal"] = {Globals::predicate_const_true, "animpoint_sit_normal"};
+        this->m_animpoint_table["animpoint_sit_normal"] = {
+            Globals::predicate_animpoint_bread, "animpoint_sit_normal_eat_bread"};
+        this->m_animpoint_table["animpoint_sit_normal"] = {
+            Globals::predicate_animpoint_kolbasa, "animpoint_sit_normal_eat_kolbasa"};
+        this->m_animpoint_table["animpoint_sit_normal"] = {
+            Globals::predicate_animpoint_vodka, "animpoint_sit_normal_drink_vodka"};
+        this->m_animpoint_table["animpoint_sit_normal"] = {
+            Globals::predicate_animpoint_energy, "animpoint_sit_normal_drink_energy"};
+        this->m_animpoint_table["animpoint_sit_normal"] = {
+            Globals::predicate_animpoint_guitar, "animpoint_sit_normal_guitar"};
+
+        this->m_animpoint_table["animpoint_sit_low"] = {Globals::predicate_const_true, "animpoint_sit_low"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_bread, "animpoint_sit_low_eat_bread"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_kolbasa, "animpoint_sit_low_eat_kolbasa"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_vodka, "animpoint_sit_low_drink_vodka"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_energy, "animpoint_sit_low_drink_energy"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_guitar, "animpoint_sit_low_guitar"};
+        this->m_animpoint_table["animpoint_sit_low"] = {
+            Globals::predicate_animpoint_harmonica, "animpoint_sit_low_harmonica"};
+
+        this->m_animpoint_table["walker_camp"] = {Globals::predicate_animpoint_guitar, "play_guitar"};
+        this->m_animpoint_table["play_harmonica"] = {Globals::predicate_animpoint_harmonica, "play_harmonica"};
 #pragma endregion
 
 #pragma region Cordis Jobs Types Initializing
