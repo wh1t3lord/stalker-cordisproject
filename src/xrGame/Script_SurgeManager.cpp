92736d6ceb860454d1ee5e4455a761fe16fc43de
phantom1020 <phantom1020@yandex.ru>
2020-02-06 09:34:45 +0300

    get_nearest_cover

--- src/xrGame/Script_SurgeManager.cpp
+++ src/xrGame/Script_SurgeManager.cpp
@@ -30,7 +30,87 @@ void Script_SurgeManager::init_surge_covers(void)
     }
 }
 
-std::uint16_t Script_SurgeManager::get_nearest_cover(void) { return std::uint16_t(); }
+std::uint16_t Script_SurgeManager::get_nearest_cover(void)
+{
+    if (this->m_is_loaded)
+        this->init_surge_covers();
+
+    xr_map<int, std::pair<CScriptGameObject*, xr_map<std::uint32_t, CondlistData>>> hides = this->m_covers;
+
+    if (this->m_count)
+    {
+        for (const std::pair<int, std::pair<CScriptGameObject*, xr_map<std::uint32_t, CondlistData>>>& it : hides)
+        {
+            if (!it.second.second.empty())
+            {
+                xr_string picked_section_name =
+                    XR_LOGIC::pick_section_from_condlist(DataBase::Storage::getInstance().getActor(),
+                        static_cast<CScriptGameObject*>(nullptr), it.second.second);
+
+                if (!picked_section_name.empty() && picked_section_name != "true")
+                {
+                    hides.erase(it.first);
+                }
+            }
+        }
+
+        std::uint16_t nearest_cover_id = hides[0].first->ID();
+        float nearest_cover_distance =
+            hides[0].first->Position().distance_to(DataBase::Storage::getInstance().getActor()->Position());
+
+        for (const std::pair<int, std::pair<CScriptGameObject*, xr_map<std::uint32_t, CondlistData>>>& it : hides)
+        {
+            if (DataBase::Storage::getInstance()
+                    .getStorage()
+                    .at(it.second.first->ID())
+                    .getClientObject()
+                    ->inside(DataBase::Storage::getInstance().getActor()->Position()))
+            {
+                return it.second.first->ID();
+            }
+
+            float distance =
+                it.second.first->Position().distance_to(DataBase::Storage::getInstance().getActor()->Position());
+
+            if (distance < nearest_cover_distance)
+            {
+                if (!it.second.second.empty())
+                {
+                    xr_string picked_section_name =
+                        XR_LOGIC::pick_section_from_condlist(DataBase::Storage::getInstance().getActor(),
+                            static_cast<CScriptGameObject*>(nullptr), it.second.second);
+                    if (picked_section_name == "true")
+                    {
+                        nearest_cover_distance = distance;
+                        nearest_cover_id = it.second.first->ID();
+                    }
+                }
+                else
+                {
+                    nearest_cover_distance = distance;
+                    nearest_cover_id = it.second.first->ID();
+                }
+            }
+        }
+
+        if (nearest_cover_id == hides[0].first->ID())
+        {
+            if (!hides[0].second.empty())
+            {
+                xr_string picked_section_name =
+                    XR_LOGIC::pick_section_from_condlist(DataBase::Storage::getInstance().getActor(),
+                        static_cast<CScriptGameObject*>(nullptr), hides[0].second);
+
+                if (!picked_section_name.empty() && picked_section_name != "true")
+                    return 0;
+            }
+        }
+
+        return nearest_cover_id;
+    }
+
+    return 0;
+}
 
 void Script_SurgeManager::update(void) {}
 
