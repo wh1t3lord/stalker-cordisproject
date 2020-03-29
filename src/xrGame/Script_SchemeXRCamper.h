2ff28f32aaa8fa88ab7d96e5ac6dd6175da80e5d
Lord <phantom1020@yandex.ru>
2020-03-29 20:42:49 +0300

    Implemneted xr_camper scheme. Continue work

---
+++ src/xrGame/Script_SchemeXRCamper.h
@@ -0,0 +1,80 @@
+#pragma once
+
+namespace Cordis
+{
+	namespace Scripts
+	{
+		class Script_EvaluatorCamperEnd : public CScriptPropertyEvaluator
+		{
+		public:
+			Script_EvaluatorCamperEnd(const xr_string& name, DataBase::Storage_Scheme& storage) : CScriptPropertyEvaluator(nullptr, name.c_str()), m_p_storage(&storage)
+			{
+
+			}
+
+			virtual _value_type evaluate(void)
+			{
+				return !XR_LOGIC::is_active(this->m_object, *this->m_p_storage);
+			}
+
+		private:
+			DataBase::Storage_Scheme* m_p_storage;
+		};
+
+		class Script_EvaluatorCloseCombat : public CScriptPropertyEvaluator
+		{
+		public:
+			Script_EvaluatorCloseCombat(const xr_string& name, DataBase::Storage_Scheme& storage) : CScriptPropertyEvaluator(nullptr, name.c_str()), m_p_storage(&storage), m_is_close_combat(false)
+			{}
+
+			virtual _value_type evaluate(void);
+		private:
+			bool m_is_close_combat;
+			DataBase::Storage_Scheme* m_p_storage;
+		};
+
+		class Script_SchemeXRCamper : public Script_ISchemeStalker
+		{
+			using inherited_scheme = Script_ISchemeStalker;
+
+		public:
+			Script_SchemeXRCamper(void) = delete;
+			Script_SchemeXRCamper(const xr_string& name, DataBase::Storage_Scheme& storage);
+			~Script_SchemeXRCamper(void);
+
+			virtual void initialize(void);
+			virtual void execute(void);
+			virtual void finalize(void);
+
+			virtual void activate_scheme(const bool is_loading, CScriptGameObject* const p_client_object);
+			virtual void reset_scheme(const bool value, CScriptGameObject* const p_client_object);
+
+			bool process_point(std::uint32_t mode, std::uint32_t number)
+			{
+				return false;
+			}
+
+		private:
+			bool process_danger(void);
+			bool on_place(void);
+			bool can_shoot(void);
+			std::pair<std::uint32_t, Fvector> get_next_point(const std::uint32_t flag);
+			void scan(const std::uint32_t flag);
+		private:
+			bool m_is_danger;
+			std::uint8_t m_flag;
+			std::uint32_t m_scantime;
+			Script_MoveManager* m_p_move_manager;
+			CScriptGameObject* m_p_enemy;
+			Fvector m_enemy_position;
+			Fvector m_look_position;
+			Fvector m_dest_position;
+			Fvector m_look_point;
+			std::pair<std::uint32_t, Fvector> m_next_point;
+			xr_map<std::uint32_t, std::pair<std::uint32_t, Fvector>> m_scan_table;	
+
+		};
+	}
+}
+
+
