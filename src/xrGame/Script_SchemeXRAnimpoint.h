11953854e6c2eadcd275c1043ae4f9cce06cb12d
Lord <phantom1020@yandex.ru>
2020-04-21 21:32:23 +0300

    Full implemented animpoint scheme

--- src/xrGame/Script_SchemeXRAnimpoint.h
+++ src/xrGame/Script_SchemeXRAnimpoint.h
@@ -53,6 +53,108 @@ namespace Cordis
 			virtual void execute(void);
 			virtual void finalize(void);
 			virtual void net_destroy(CScriptGameObject* const p_client_object);
+
+			static inline void add_to_binder(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
+				const xr_string& scheme_name, const xr_string& section_name, DataBase::Storage_Scheme& storage)
+			{
+				if (p_client_object == nullptr)
+				{
+					MESSAGEWR("Invalid npc!");
+					return;
+				}
+
+				if (p_ini == nullptr)
+				{
+					MESSAGEWR("invalid ini!");
+					return;
+				}
+
+				xr_map<xr_string, std::uint32_t> operators;
+				xr_map<xr_string, std::uint32_t> properties;
+
+				CScriptActionPlanner* const p_planner = Globals::get_script_action_planner(p_client_object);
+
+				properties["need_animpoint"] = Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kAnimpointProperty + 1;
+				properties["reach_animpoint"] = Globals::XR_ACTIONS_ID::kAnimationPointAction + 2;
+				properties["state_mgr_logic_active"] = Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kStateManager + 4;
+
+				operators["action_animpoint"] = Globals::XR_ACTIONS_ID::kAnimationPointAction + 1;
+				operators["action_reach_animpoint"] = Globals::XR_ACTIONS_ID::kAnimationPointAction + 2;
+
+				p_planner->add_evaluator(properties.at("need_animpoint"), new Script_EvaluatorNeedAnimpoint("animpoint_need", storage));
+				p_planner->add_evaluator(properties.at("reach_animpoint"), new Script_EvaluatorReachAnimpoint("animpoint_reach", storage));
+
+
+				storage.setAnimpoint(new Script_Animpoint(p_client_object->ID(), storage));
+				DataBase::Storage::getInstance().setStorageSchemesActions(p_client_object->ID(), scheme_name, storage.getAnimpoint());
+
+				Script_ActionReachAnimpoint* const p_scheme_reach = new Script_ActionReachAnimpoint("action_reach_animpoint", storage);
+				p_scheme_reach->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyAlive, true));
+				p_scheme_reach->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyAnomaly, false));
+				p_scheme_reach->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyEnemy, false));
+				p_scheme_reach->add_condition(CWorldProperty(properties.at("need_animpoint"), true));
+				p_scheme_reach->add_condition(CWorldProperty(properties.at("reach_animpoint"), false));
+				p_scheme_reach->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kStoheMeetBase + 1, false));
+				p_scheme_reach->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kSidorWoundedBase, false));
+				p_scheme_reach->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kAbuseBase, false));
+				p_scheme_reach->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kWoundedExist, false));
+				p_scheme_reach->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kCorpseExist, false));
+				p_scheme_reach->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyItems, false));
+
+				p_scheme_reach->add_effect(CWorldProperty(properties.at("need_animpoint"), false));
+				p_scheme_reach->add_effect(CWorldProperty(properties.at("state_mgr_logic_active"), false));
+				p_planner->add_operator(operators.at("action_reach_animpoint"), p_scheme_reach);
+				DataBase::Storage::getInstance().setStorageSchemesActions(p_client_object->ID(), scheme_name, p_scheme_reach);
+
+				Script_SchemeXRAnimpoint* const p_scheme = new Script_SchemeXRAnimpoint("action_animpoint", storage);
+				p_scheme->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyAlive, true));
+				p_scheme->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyAnomaly, false));
+				p_scheme->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyEnemy, false));
+				p_scheme->add_condition(CWorldProperty(properties.at("need_animpoint"), true));
+				p_scheme->add_condition(CWorldProperty(properties.at("reach_animpoint"), true));
+				p_scheme->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kStoheMeetBase + 1, false));
+				p_scheme->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kSidorWoundedBase, false));
+				p_scheme->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kAbuseBase, false));
+				p_scheme->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kWoundedExist, false));
+				p_scheme->add_condition(CWorldProperty(Globals::XR_ACTIONS_ID::XR_EVALUATORS_ID::kCorpseExist, false));
+				p_scheme->add_condition(CWorldProperty(StalkerDecisionSpace::eWorldPropertyItems, false));
+
+				p_scheme->add_effect(CWorldProperty(properties.at("need_animpoint"), false));
+				p_scheme->add_effect(CWorldProperty(properties.at("state_mgr_logic_active"), false));
+				p_planner->add_operator(operators.at("action_animpoint"), p_scheme);
+				DataBase::Storage::getInstance().setStorageSchemesActions(p_client_object->ID(), scheme_name, p_scheme);
+
+				p_planner->action(Globals::XR_ACTIONS_ID::kAlife).add_condition(CWorldProperty(properties.at("need_animpoint"), false));
+			}
+
+			static inline void set_scheme(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
+				const xr_string& scheme_name, const xr_string& section_name, const xr_string& gulag_name)
+			{
+				DataBase::Storage_Scheme* const p_storage = XR_LOGIC::assign_storage_and_bind(p_client_object, p_ini, scheme_name, section_name, gulag_name);
+
+				p_storage->setLogic(XR_LOGIC::cfg_get_switch_conditions(p_ini, section_name, p_client_object));
+				xr_string cover_name = Globals::Utils::cfg_get_string(p_ini, section_name, "cover_name");
+				if (cover_name.empty())
+					cover_name = "$script_id$_cover";
+				p_storage->setXRAnimpointCoverName(cover_name);
+				p_storage->setXRAnimpointUseCamp(Globals::Utils::cfg_get_bool(p_ini, section_name, "use_camp"));
+				float reach_distance = Globals::Utils::cfg_get_number(p_ini, section_name, "reach_distance");
+
+				if (fis_zero(reach_distance))
+					reach_distance = 0.75f;
+
+				reach_distance *= reach_distance;
+				p_storage->setXRAnimpointReachDistance(reach_distance);
+				
+				xr_string movement_name = Globals::Utils::cfg_get_string(p_ini, section_name, "reach_movement");
+
+				if (movement_name.empty())
+					movement_name = "walk";
+
+				p_storage->setXRAnimpointReachMovementName(movement_name);
+
+				p_storage->setXRAnimpointAvailAnimations(Globals::Utils::parse_names(Globals::Utils::cfg_get_string(p_ini, section_name, "avail_animations")));
+			}
 		};
 
 		class Script_ActionReachAnimpoint : public Script_ISchemeStalker
