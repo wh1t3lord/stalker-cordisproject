49e28d4fe26ee7ed266667068ffd2920d0eed6b3
Lord <phantom1020@yandex.ru>
2020-05-06 11:12:47 +0300

    Skipped implementation of helicopter, continue somewhere, I need to to test the game fastly

--- src/xrGame/Script_Binder_Helicopter.cpp
+++ src/xrGame/Script_Binder_Helicopter.cpp
@@ -5,6 +5,45 @@ namespace Cordis
 {
 	namespace Scripts
 	{
+		// Lord: дописать когда протестируются все основные схемы!
+		// Lord: суть в том что в этом биндере используется схема heli_combat, которую я не реализовал, а нужно!
+		Script_Binder_Helicopter::Script_Binder_Helicopter(CScriptGameObject* const p_client_object) : CScriptBinderObject(p_client_object), m_is_loaded(false), m_is_initialized(false), m_p_helicopter(Script_HelicopterFireStorage::getInstance().AllocateFirer(p_client_object)), m_p_ini(p_client_object->spawn_ini())
+		{
+		}
 
+		Script_Binder_Helicopter::~Script_Binder_Helicopter(void)
+		{
+		}
+
+		void Script_Binder_Helicopter::reinit()
+		{
+			CScriptBinderObject::reinit();
+		}
+
+		bool Script_Binder_Helicopter::net_Spawn(SpawnType DC)
+		{
+			return false;
+		}
+
+		void Script_Binder_Helicopter::net_Destroy()
+		{
+		}
+
+		void Script_Binder_Helicopter::shedule_Update(u32 time_delta)
+		{
+		}
+
+		void Script_Binder_Helicopter::save(NET_Packet* output_packet)
+		{
+		}
+
+		void Script_Binder_Helicopter::load(IReader* input_packet)
+		{
+		}
+
+		bool Script_Binder_Helicopter::net_SaveRelevant()
+		{
+			return true;
+		}
 	}
 }
