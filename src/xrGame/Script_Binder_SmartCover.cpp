9d5ca43ca6a468b17e93b5cec742a9630f73ddaa
Lord <>
2020-05-17 12:57:08 +0300

    Некоторые изменения

--- src/xrGame/Script_Binder_SmartCover.cpp
+++ src/xrGame/Script_Binder_SmartCover.cpp
@@ -15,19 +15,14 @@ bool Cordis::Scripts::Script_Binder_SmartCover::net_Spawn(SpawnType DC)
 		return false;
 
 	DataBase::Storage::getInstance().setSmartCoversByName(this->m_object->Name(), this->m_object);
-#ifdef DEBUG
 	MESSAGE("smart_cover %s is registered!", this->m_object->Name());
-#endif // DEBUG
 	return true;
 }
 
 void Cordis::Scripts::Script_Binder_SmartCover::net_Destroy()
 {
 	DataBase::Storage::getInstance().setSmartCoversByName(this->m_object->Name(), nullptr);
-
-#ifdef DEBUG
 	MESSAGEI("smart_cover %s is unregistered!", this->m_object->Name());
-#endif // DEBUG
 
 	CScriptBinderObject::net_Destroy();
 }
