ce50b4814cbb74ebb62771eae74265d8c35a06d0
phantom1020 <phantom1020@yandex.ru>
2020-05-23 08:11:35 +0300

    merged

--- src/xrGame/Script_Binder_Helicopter.cpp
+++ src/xrGame/Script_Binder_Helicopter.cpp
@@ -1,2 +1,31 @@
 #include "stdafx.h"
 #include "Script_Binder_Helicopter.h"
+
+namespace Cordis
+{
+    namespace Scripts
+{
+    Script_Binder_Helicopter::Script_Binder_Helicopter(CScriptGameObject* const p_client_object) : inherited(p_client_object), m_is_initialized(false), m_is_loaded(false) 
+    {
+        this->m_p_heli_logic = Script_HelicopterFireStorage::getInstance().AllocateFirer(p_client_object);
+    }
+
+    Script_Binder_Helicopter::~Script_Binder_Helicopter(void) {}
+
+    void Script_Binder_Helicopter::reinit() {}
+
+    void Script_Binder_Helicopter::reload(LPCSTR section) {}
+
+    bool Script_Binder_Helicopter::net_Spawn(SpawnType DC) { return false; }
+
+    void Script_Binder_Helicopter::net_Destroy() {}
+
+    void Script_Binder_Helicopter::shedule_Update(u32 time_delta) {}
+
+    void Script_Binder_Helicopter::save(NET_Packet* output_packet) {}
+
+    void Script_Binder_Helicopter::load(IReader* input_packet) {}
+
+    bool Script_Binder_Helicopter::net_SaveRelevant() { return true; }
+    } // namespace Scripts
+}
--- src/xrGame/Script_Binder_Helicopter.h
+++ src/xrGame/Script_Binder_Helicopter.h
@@ -1,4 +1,36 @@
 #pragma once
-class Script_Binder_Helicopter
+
+namespace Cordis
 {
+
+namespace Scripts
+{
+class Script_Binder_Helicopter : public CScriptBinderObject
+{
+    using inherited = CScriptBinderObject;
+
+public:
+    Script_Binder_Helicopter(void) = delete;
+    Script_Binder_Helicopter(CScriptGameObject* const p_client_object);
+    ~Script_Binder_Helicopter(void);
+
+    virtual void reinit();
+    virtual void reload(LPCSTR section);
+    virtual bool net_Spawn(SpawnType DC);
+    virtual void net_Destroy();
+    virtual void shedule_Update(u32 time_delta);
+    virtual void save(NET_Packet* output_packet);
+    virtual void load(IReader* input_packet);
+    virtual bool net_SaveRelevant();
+    
+    //void on_hit(float power, float impulse, )
+
+private:
+    bool m_is_initialized;
+    bool m_is_loaded;
+    CScriptIniFile* m_p_ini;
+    Script_HelicopterFire* m_p_heli_logic;
 };
+}
+} // namespace Cordis
+
