08e07a7001f9c3018bbe8f1a6b528a507670b86f
Lord <phantom1020@yandex.ru>
2020-06-23 11:10:27 +0300

    исправлен ph_on_hit.cpp

--- src/xrGame/Script_SchemePHIdle.cpp
+++ src/xrGame/Script_SchemePHIdle.cpp
@@ -5,8 +5,8 @@ namespace Cordis
 {
 namespace Scripts
 {
-Script_SchemePHIdle::Script_SchemePHIdle(CScriptGameObject* const p_client_object, void* storage)
-    : inherited_scheme(p_client_object, storage)
+Script_SchemePHIdle::Script_SchemePHIdle(CScriptGameObject* const p_client_object, DataBase::Script_ComponentScheme_PHIdle* storage)
+    : inherited_scheme(p_client_object, storage), m_p_storage(storage)
 {
     this->m_scheme_name = "ph_idle";
 }
@@ -17,21 +17,20 @@ void Script_SchemePHIdle::reset_scheme(const bool value, CScriptGameObject* cons
 {
     Msg("[Scripts/Script_SchemePHIdle/reset_scheme(is_loading, p_client_object)] %s", this->m_npc->Name());
 
-    this->m_npc->SetNonscriptUsable(this->m_p_storage->IsPHIdleNonScriptUsable());
+    this->m_npc->SetNonscriptUsable(this->m_p_storage->IsNonScriptUsable());
 }
 
 void Script_SchemePHIdle::update(const float delta)
 {
     if (XR_LOGIC::try_switch_to_another_section(
-            this->m_npc, *this->m_p_storage, DataBase::Storage::getInstance().getActor()))
+            this->m_npc, this->m_p_storage, DataBase::Storage::getInstance().getActor()))
         return;
 }
 
 void Script_SchemePHIdle::hit_callback(CScriptGameObject* const p_client_object, const float amount,
     const Fvector& local_direction, CScriptGameObject* const p_client_who, const std::int16_t bone_index)
 {
-    Msg("[Scripts/Script_SchemePHIdle/hit_callback(p_client_object, amount, local_direction, p_client_who, "
-        "bone_index)] %s %d %s %d",
+    MESSAGE("%s %d %s %d",
         p_client_object->Name(), amount, p_client_who ? p_client_who->Name() : "Unknown", bone_index);
 
     if (!this->m_p_storage->getHitOnBone().at(bone_index).empty())
@@ -66,8 +65,8 @@ void Script_SchemePHIdle::set_scheme(CScriptGameObject* const p_client_object, C
         return;
     }
 
-    DataBase::Storage_Scheme* p_storage =
-        XR_LOGIC::assign_storage_and_bind(p_client_object, p_ini, scheme_name, section_name, gulag_name);
+    DataBase::Script_ComponentScheme_PHIdle* p_storage =
+        XR_LOGIC::assign_storage_and_bind<DataBase::Script_ComponentScheme_PHIdle>(p_client_object, p_ini, scheme_name, section_name, gulag_name);
 
     if (!p_storage)
     {
@@ -76,7 +75,7 @@ void Script_SchemePHIdle::set_scheme(CScriptGameObject* const p_client_object, C
     }
 
     p_storage->setLogic(XR_LOGIC::cfg_get_switch_conditions(p_ini, section_name, p_client_object));
-    p_storage->setPHIdleNonScriptUsable(Globals::Utils::cfg_get_bool(p_ini, section_name, "nonscript_usable"));
+    p_storage->setNonScriptUsable(Globals::Utils::cfg_get_bool(p_ini, section_name, "nonscript_usable"));
     p_storage->setTipName(Globals::Utils::cfg_get_string(p_ini, section_name, "tips"));
     p_storage->setOnUseCondlist(XR_LOGIC::cfg_get_condlist(p_ini, section_name, "on_use", p_client_object).getCondlist());
     p_storage->setHitOnBone(Globals::Utils::parse_data_1v(
