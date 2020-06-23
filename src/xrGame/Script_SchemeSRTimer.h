fee5344c0dbe8c5d5275d6106bbdbda362be2c56
Lord <phantom1020@yandex.ru>
2020-06-23 13:14:01 +0300

    Исправлен sr_timer.cpp

--- src/xrGame/Script_SchemeSRTimer.h
+++ src/xrGame/Script_SchemeSRTimer.h
@@ -9,7 +9,7 @@ class Script_SchemeSRTimer : public Script_ISchemeEntity
 
 public:
     Script_SchemeSRTimer(void) = delete;
-    Script_SchemeSRTimer(CScriptGameObject* const p_client_object, void* storage);
+    Script_SchemeSRTimer(CScriptGameObject* const p_client_object, DataBase::Script_ComponentScheme_SRTimer* storage);
     ~Script_SchemeSRTimer(void);
 
     virtual void update(const float delta);
@@ -17,7 +17,7 @@ public:
 
     // @ PRIVATE uses, in XR_LOGIC
     static inline void add_to_binder(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
-        const xr_string& scheme_name, const xr_string& section_name, void* storage)
+        const xr_string& scheme_name, const xr_string& section_name, DataBase::Script_IComponentScheme* storage)
     {
         if (!p_client_object)
         {
@@ -31,17 +31,19 @@ public:
             return;
         }
 
-        Msg("[Scripts/add_to_binder(p_client_object, p_ini, scheme_name, section_name, storage)] added "
-            "Script_SchemeMobWalker scheme to binder, name=%s scheme=%s section=%s",
+        MESSAGEI("added scheme to binder, name=%s scheme=%s section=%s",
             p_client_object->Name(), scheme_name.c_str(), section_name.c_str());
 
-        Script_ISchemeEntity* p_scheme = new Script_SchemeSRTimer(p_client_object, storage);
+        Script_ISchemeEntity* p_scheme = new Script_SchemeSRTimer(p_client_object, static_cast<DataBase::Script_ComponentScheme_SRTimer*>(storage));
         DataBase::Storage::getInstance().setStorageSchemesActions(p_client_object->ID(), scheme_name, p_scheme);
     }
 
     // @ PRIVATE, uses in XR_LOGIC
     static void set_scheme(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
         const xr_string& scheme_name, const xr_string& section_name, const xr_string& gulag_name);
+
+private:
+    DataBase::Script_ComponentScheme_SRTimer* m_p_storage;
 };
 }
 } // namespace Cordis
