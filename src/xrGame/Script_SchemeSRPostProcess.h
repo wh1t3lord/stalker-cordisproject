0408c884284249499cb5db9230242cc79f589f54
Lord <phantom1020@yandex.ru>
2020-06-23 11:36:43 +0300

    исправлен sr_post_process

--- src/xrGame/Script_SchemeSRPostProcess.h
+++ src/xrGame/Script_SchemeSRPostProcess.h
@@ -11,7 +11,7 @@ class Script_SchemeSRPostProcess : public Script_ISchemeEntity
 
 public:
     Script_SchemeSRPostProcess(void) = delete;
-    Script_SchemeSRPostProcess(CScriptGameObject* const p_client_object, void* storage);
+	Script_SchemeSRPostProcess(CScriptGameObject* const p_client_object, DataBase::Script_ComponentScheme_SRPostProcess* storage);
     ~Script_SchemeSRPostProcess(void);
 
     virtual void reset_scheme(const bool value, CScriptGameObject* const p_client_object);
@@ -20,7 +20,7 @@ public:
  
     // @ PRIVATE uses, in XR_LOGIC
     static inline void add_to_binder(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
-        const xr_string& scheme_name, const xr_string& section_name, void* storage)
+        const xr_string& scheme_name, const xr_string& section_name, DataBase::Script_IComponentScheme* storage)
     {
         if (!p_client_object)
         {
@@ -34,11 +34,10 @@ public:
             return;
         }
 
-        Msg("[Scripts/add_to_binder(p_client_object, p_ini, scheme_name, section_name, storage)] added "
-            "Script_SchemeMobWalker scheme to binder, name=%s scheme=%s section=%s",
+        MESSAGEI("added scheme to binder, name=%s scheme=%s section=%s",
             p_client_object->Name(), scheme_name.c_str(), section_name.c_str());
 
-        Script_ISchemeEntity* p_scheme = new Script_SchemeSRPostProcess(p_client_object, storage);
+        Script_ISchemeEntity* p_scheme = new Script_SchemeSRPostProcess(p_client_object, reinterpret_cast<DataBase::Script_ComponentScheme_SRPostProcess*>(storage));
         DataBase::Storage::getInstance().setStorageSchemesActions(p_client_object->ID(), scheme_name, p_scheme);
     }
 
@@ -58,6 +57,7 @@ private:
     float m_intensity_base;
     float m_hit_power;
     float m_intensity_inertion;
+    DataBase::Script_ComponentScheme_SRPostProcess* m_p_storage;
     SPPInfo::SNoise m_noise;
     SPPInfo::SColor m_gray_color;
     SPPInfo::SColor m_base_color;
