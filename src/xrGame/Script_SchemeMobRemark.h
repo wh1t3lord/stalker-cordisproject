4a0abba48b4fca1e119be0e30e7e0ecfa2024e56
Lord <phantom1020@yandex.ru>
2020-06-24 09:39:00 +0300

    фиксы

--- src/xrGame/Script_SchemeMobRemark.h
+++ src/xrGame/Script_SchemeMobRemark.h
@@ -17,7 +17,7 @@ public:
 
     // @ PRIVATE uses, in XR_LOGIC
     static inline void add_to_binder(CScriptGameObject* const p_client_object, CScriptIniFile* const p_ini,
-        const xr_string& scheme_name, const xr_string& section_name, DataBase::Script_ComponentScheme_MobRemark* storage)
+        const xr_string& scheme_name, const xr_string& section_name, DataBase::Script_IComponentScheme* storage)
     {
         if (!p_client_object)
         {
@@ -34,7 +34,7 @@ public:
         MESSAGEI("added scheme to binder, name=%s scheme=%s section=%s",
             p_client_object->Name(), scheme_name.c_str(), section_name.c_str());
 
-        Script_ISchemeEntity* action = new Script_SchemeMobRemark(p_client_object, reinterpret_cast<DataBase::Script_ComponentScheme_MobRemark*>(storage));
+        Script_ISchemeEntity* action = new Script_SchemeMobRemark(p_client_object, static_cast<DataBase::Script_ComponentScheme_MobRemark*>(storage));
         DataBase::Storage::getInstance().setStorageSchemesActions(
             p_client_object->ID(), action->getSchemeName(), action);
     }
