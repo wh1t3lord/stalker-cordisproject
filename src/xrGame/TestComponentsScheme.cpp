4071fca9f036d9be79c6ff2d68d94943b5a8d31e
Lord <phantom1020@yandex.ru>
2020-06-25 10:26:07 +0300

    Тестирование set/get методов у SRIdle

--- src/xrGame/TestComponentsScheme.cpp
+++ src/xrGame/TestComponentsScheme.cpp
@@ -3,13 +3,32 @@
 #include <src\gtest-all.cc>
 
 #define CurrentTest TestComponentsScheme
+using namespace Cordis;
+using namespace Cordis::Scripts;
 using namespace Cordis::Scripts::DataBase;
-GTEST_TEST(CurrentTest, TestComponent_SRIdle) {
+
+GTEST_TEST(CurrentTest, TestComponent_SRIdle_SetGet) {
 	Script_ComponentScheme_SRIdle instance;
 	instance.setActionSchemeID(15);
 	CGameObject obj;
 	CScriptGameObject object(&obj);
 	instance.setClientObject(&object);
+	instance.setEnabled(true);
+	instance.setLogic({LogicData(), LogicData()});
+	instance.setLogicName("test");
+	instance.setSchemeName("scheme");
+	instance.setSignals("test", true);
 
-	instance.clear();
+
+	EXPECT_TRUE(instance.getSignals().at("test") == true);
+	EXPECT_TRUE(instance.getSchemeName() == "scheme");
+	EXPECT_TRUE(instance.getLogicName() == "test");
+	EXPECT_TRUE(instance.getLogic().size() == 2);
+	EXPECT_TRUE(instance.IsEnabled());
+	EXPECT_FALSE(instance.isCalledClear());
+	EXPECT_TRUE(instance.getClientObject());
+}
+
+GTEST_TEST(CurrentTest, Test_CGameObject) {
+	CGameObject instance;
 }
