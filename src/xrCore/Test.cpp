10956606413cb1d757620a39bc4f4e20717eeffc
Lord <phantom1020@yandex.ru>
2020-06-25 10:31:31 +0300

    Тестирование xr_string

--- src/xrCore/Test.cpp
+++ src/xrCore/Test.cpp
@@ -4,6 +4,26 @@
 
 #define CurrentTest Test_xrCore
 
-GTEST_TEST(CurrentTest, TestCore) {
-	EXPECT_EQ(0, 0);
+GTEST_TEST(CurrentTest, xr_string) {
+	xr_string str;
+	str = "data";
+
+	EXPECT_TRUE(str == "data");
+
+	EXPECT_FALSE(str.empty());
+
+	EXPECT_TRUE(str.find('d') != xr_string::npos);
+
+	EXPECT_TRUE(str[0] == 'd');
+	EXPECT_TRUE(str[1] == 'a');
+	EXPECT_TRUE(str[2] == 't');
+	EXPECT_TRUE(str[3] == 'a');
+
+	str.append("_something");
+
+	EXPECT_TRUE(str == "data_something");
+
+	str.clear();
+
+	EXPECT_TRUE(str.empty());
 }
