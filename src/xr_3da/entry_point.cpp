5d96e0dc5a7b3c93db8881ba73e094d3e05bac0e
Lord <phantom1020@yandex.ru>
2020-06-26 09:01:13 +0300

    Поправлен проект

--- src/xr_3da/entry_point.cpp
+++ src/xr_3da/entry_point.cpp
@@ -17,10 +17,9 @@
 
 int entry_point(pcstr commandLine)
 {
-    splash::show(true);
-    Device.Initialize(commandLine);
     R_ASSERT3(SDL_Init(SDL_INIT_VIDEO) == 0, "Unable to initialize SDL", SDL_GetError());
-
+    Device.Initialize(commandLine);
+    splash::show(true);
     Cordis::TaskManager::getInstance().getCore()->run([&]() {
 		xrDebug::Initialize();
 
