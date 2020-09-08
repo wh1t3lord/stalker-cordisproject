076f07981d73020fa0ce4edf8ddc51ef24af1fb5
Paweł <68504048+diamondhat@users.noreply.github.com>
2020-09-08 20:41:54 +0300

    DEL: удалены ненужные папки

--- src/xrEngine_sdk/Render.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdafx.h"
-#include "Render.h"
-
-// resources
-IRender_Light::~IRender_Light() { GEnv.Render->light_destroy(this); }
-IRender_Glow::~IRender_Glow() { GEnv.Render->glow_destroy(this); }
