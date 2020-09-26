6b283495bf3404e9c4ca8f3e3cfed5c17acec2af
NoName <>
2020-09-26 16:31:42 +0300

    FIX: исправлено сравнение для evaluator

--- src/xrGame/Script_StateEvaluators.h
+++ src/xrGame/Script_StateEvaluators.h
@@ -659,10 +659,10 @@ public:
 
     virtual _value_type evaluate(void)
     {
-        return ((!Script_GlobalHelper::getInstance()
+        return ((Script_GlobalHelper::getInstance()
                         .getStateLibrary()
                         .at(this->m_p_state_manager->getTargetStateName())
-                        .getMentalType()) ||
+                        .getMentalType() == Globals::kUnsignedInt32Undefined) ||
             (Script_GlobalHelper::getInstance()
                     .getStateLibrary()
                     .at(this->m_p_state_manager->getTargetStateName())
