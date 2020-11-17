c5391ad4b5d49dc26944a01cb11c2d3248404d8f
diamondhat <servetowhites@yandex.ru>
2020-11-17 15:07:27 +0300

    ADD: написано несколько тестов для SR схем для проверки метода clear

--- src/xrGame/TestComponentsScheme.cpp
+++ src/xrGame/TestComponentsScheme.cpp
@@ -37,3 +37,105 @@ GTEST_TEST(CurrentTest, Test_CScriptGameObject) {
 	CGameObject owner;
 	CScriptGameObject instance(&owner);
 }
+
+/// <summary>
+/// Here we test Script_ComponentScheme_SRIdle clear method
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+GTEST_TEST(CurrentTest, Test_ClearMethodCorrectness_SRIdle) {
+	Script_ComponentScheme_SRIdle instance;
+	instance.clear();
+}
+
+/// <summary>
+/// Here we test Script_IComponentScheme clear method
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+GTEST_TEST(CurrentTest, Test_ClearMethodCorrectness_Script_InterfaceComponentSheme) {
+	Script_IComponentScheme instance;
+	instance.setActionSchemeID(1);
+	instance.setSignals("test", true);
+
+	CScriptIniFile ini("", false, false, false);
+	instance.setIni(&ini);
+	instance.setSchemeName("scheme");
+	instance.setLogicName("logic_name");
+	instance.setEnabled(true);
+
+	CGameObject obj_game;
+	CScriptGameObject obj(&obj_game);
+	instance.setClientObject(&obj);
+
+	LogicData element;
+	xr_vector<LogicData> logic;
+	logic.push_back(element);
+	instance.setLogic(logic);
+	instance.addAction(new Script_ISchemeEntity(nullptr, nullptr));
+
+	instance.clear();
+
+	EXPECT_TRUE(instance.IsEnabled() == false);
+	EXPECT_TRUE(instance.getSchemeName().empty());
+	EXPECT_TRUE(instance.getIni() == nullptr);
+	EXPECT_TRUE(instance.getClientObject() == nullptr);
+	EXPECT_TRUE(instance.getSignals().empty());
+	EXPECT_TRUE(instance.getLogic().empty());
+	EXPECT_TRUE(instance.getLogicName().empty());
+	EXPECT_TRUE(instance.isCalledClear() == true);
+	EXPECT_TRUE(instance.getActions().empty());
+}
+
+/// <summary>
+/// Here we test Script_ComponentScheme_SRLight clear method
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+GTEST_TEST(CurrentTest, Test_ClearMethodCorrectness_SRLight) {
+	Script_ComponentScheme_SRLight instance;
+	instance.setLight(true);
+
+	instance.clear();
+	EXPECT_FALSE(instance.isLight());
+}
+
+/// <summary>
+/// Here we test Script_ComponentScheme_SRParticle clear method
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+GTEST_TEST(CurrentTest, Test_ClearMethodCorrectness_SRParticle) {
+	Script_ComponentScheme_SRParticle instance;
+
+	instance.setMode(12);
+	instance.setName("test");
+	instance.setPathName("heh");
+	instance.setLooped(true);
+
+	instance.clear();
+
+	EXPECT_TRUE(instance.getMode() == 0);
+	EXPECT_TRUE(instance.getName().empty());
+	EXPECT_TRUE(instance.getPathName().empty());
+	EXPECT_FALSE(instance.isLooped());
+}
+
+/// <summary>
+/// Here we test Script_ComponentScheme_SRPostProcess clear method
+/// </summary>
+/// <param name=""></param>
+/// <param name=""></param>
+GTEST_TEST(CurrentTest, Test_ClearMethodCorrectness_SRPostProcess) {
+	Script_ComponentScheme_SRPostProcess instance;
+
+	instance.setIntensity(std::numeric_limits<float>::max());
+	instance.setIntensitySpeed(std::numeric_limits<float>::max());
+	instance.setHitIntensity(std::numeric_limits<float>::max());
+
+	instance.clear();
+
+	EXPECT_TRUE(fis_zero(instance.getIntensity()));
+	EXPECT_TRUE(fis_zero(instance.getIntensitySpeed()));
+	EXPECT_TRUE(fis_zero(instance.getHitIntensity()));
+}
